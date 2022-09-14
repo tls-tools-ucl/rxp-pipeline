d807f362e3771b9fa5e171a982d79464c8640063
philwilkes <p.wilkes@ucl.ac.uk>
2022-09-14 10:12:10 +0100

    reads dtype from header

--- ply_io.py
+++ ply_io.py
@@ -24,7 +24,7 @@ def read_ply(fp):
                     fmt = 'ascii' 
             if 'element vertex' in line: N = int(line.split()[2])
             if 'property' in line: 
-                dtype.append(dtype_map[line.split()[1]])
+                dtype.append(dtype_map[line.split()[1]] if line.split()[1] in dtype_map.keys() else line.split()[1])
                 prop.append(line.split()[2])
             if 'element face' in line:
                 raise Exception(fp + ' appears to be a mesh')
