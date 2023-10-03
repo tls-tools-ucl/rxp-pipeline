2f97dd234088a8aa45d63b87c3b4a8cfe31f9573
philwilkes <p.wilkes@ucl.ac.uk>
2023-10-03 10:54:13 +0100

    added global rotation option

--- mat2shp.py
+++ mat2shp.py
@@ -8,14 +8,19 @@ from shapely.geometry import Point
 parser = argparse.ArgumentParser()
 parser.add_argument('-m','--mdir', required=True, type=str, help='directory where matrix files are stored')
 parser.add_argument('-o','--oshp', required=True, type=str, help='output shapefile')
+parser.add_argument('--global-matrix', required=False, type=str, default=False, help='global rotation matrix')
 parser.add_argument('--verbose', action='store_true', help='print something')
 args = parser.parse_args()
 
 df = gp.GeoDataFrame(columns=['x', 'y', 'z'])
 
-for i, dat in enumerate(glob.glob(os.path.join(args.mdir, '*.dat')) + 
+for i, dat in enumerate(glob.glob(os.path.join(args.mdir, '*.dat')) +
                         glob.glob(os.path.join(args.mdir, '*.DAT'))):
     df.loc[i, :] = np.loadtxt(dat)[:3, 3]
+
+if args.global_matrix:
+    df.loc[:, 'a'] = 1
+    df[['x', 'y', 'z']] = np.dot(np.loadtxt(args.global_matrix), df[['x', 'y', 'z', 'a']].values.T).T[:, :3]
 
 geometry = [Point(r.x, r.y) for r in df.itertuples()]
 df.geometry = geometry
@@ -24,3 +29,4 @@ X = df.unary_union.minimum_rotated_rectangle
 gp.GeoDataFrame(data=['A'], geometry=[X], columns=['A']).to_file(args.oshp)
 
 if args.verbose: print(f'shapefile saved to: {args.oshp}')
+
